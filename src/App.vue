7866fdfb50416bc1c969ee42e1fc076a24037a32
monsieurluge <monsieurluge@gmail.com>
2024-09-18 11:06:26 +0200

    styling

--- src/App.vue
+++ src/App.vue
@@ -15,17 +15,18 @@ export default {
 </script>
 
 <style>
-#app {}
+#app {
+    height: 100%;
+}
 
 header {
     height: 60px;
-    margin-bottom: 60px;
-    background-color: #f0f0f0;
+    background-color: var(--b-low);
 }
 
 h1 {
     margin: 0 0 0 1.4rem;
-    color: #2c3e50;
+    color: var(--f-high);
     font-size: 1.4rem;
     font-weight: lighter;
     line-height: 60px;
--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -1,10 +1,11 @@
 <template>
     <section class="tasks-list">
+        <div class="separator top-separator"></div>
         <h2>today</h2>
         <form @submit.prevent="addTask">
             <input type="text" placeholder="+ add a task" v-model="name" />
         </form>
-        <ul>
+        <ul v-show="store.tasks.length > 0">
             <li
                 v-for="task in store.tasks"
                 :key="task.name"
@@ -12,6 +13,7 @@
                 @click="() => store.toggleCompletion(task.name)"
             >{{ task.name }}</li>
         </ul>
+        <div class="separator bottom-separator"></div>
     </section>
 </template>
 
@@ -31,16 +33,43 @@ function addTask() {
 
 <style scoped>
 section {
+    width: 400px;
+    height: calc(100% - var(--header-height));
+    margin: 0 auto;
     display: flex;
     flex-direction: column;
-    align-items: center;
-    width: 400px;
-    margin: 0 auto;
 }
 
-form {
-    width: 100%;
-    margin-bottom: 10px;
+.separator {
+    --dot-bg: var(--background);
+    --dot-color: var(--f-low);
+    --dot-size: 2px;
+    --dot-space: 10px;
+    background-color: var(--background);
+    background:
+		linear-gradient(90deg, var(--dot-bg) calc(var(--dot-space) - var(--dot-size)), transparent 1%) center / var(--dot-space) var(--dot-space),
+		linear-gradient(var(--dot-bg) calc(var(--dot-space) - var(--dot-size)), transparent 1%) center / var(--dot-space) var(--dot-space),
+		var(--dot-color);
+}
+
+.top-separator {
+    height: 56px;
+    margin: 10px 0 10px 3px;
+}
+
+.bottom-separator {
+    flex: 1;
+    min-height: 60px;
+    margin: 10px 0 0 3px;
+}
+
+h2 {
+    margin: 0 0 3px;
+    padding: 10px;
+    font-size: 1rem;
+    font-family: monospace, sans;
+    background-color: var(--b-low);
+    border-radius: 5px;
 }
 
 input {
@@ -48,36 +77,45 @@ input {
     padding: 10px;
     font-size: 1em;
     font-family: monospace, sans;
-    border: 1px solid transparent;
+    background-color: var(--b-low);
+    border: none;
     border-radius: 5px;
     box-sizing: border-box;
 }
 
 input:hover,
 input:focus {
-    border-color: #ccc;
+    background-color: var(--b-med);
 }
 
 ul {
     width: 100%;
-    margin: 0;
+    margin: 3px 0 0 0;
     padding: 0;
     list-style: none;
 }
 
 li {
     width: 100%;
-    margin-bottom: 10px;
     padding: 10px;
     font-size: 1em;
     font-family: monospace, sans;
-    border: 1px solid #ccc;
+    background-color: var(--b-low);
     border-radius: 5px;
     overflow-x: hidden;
     text-overflow: ellipsis;
     white-space: nowrap;
     cursor: pointer;
     box-sizing: border-box;
+    transition: background-color 0.3s;
+}
+
+li:not(:last-child) {
+    margin-bottom: 3px;
+}
+
+li:hover {
+    background-color: var(--b-med);
 }
 
 .done {
